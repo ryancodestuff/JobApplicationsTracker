e61904b884c551896ca0a4085ae0ceccc1a6e9b7
ryancodestuff <rf471@student.aru.ac.uk>
2024-08-15 15:51:56 +0100

    test commit

--- JobApplicationsTracker/Form1.Designer.cs
+++ JobApplicationsTracker/Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Add Entry";
+            this.button1.Text = "dss";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
